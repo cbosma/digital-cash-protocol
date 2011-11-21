e19d68145289bb4b53f0cb61c6e114bdd4c2f8ad
marbaugh <marbaugh@d4aabf46-09eb-ed3a-424b-fb80b3e2f78e>
2011-11-21 02:48:53 +0000

    //The amount of the transaction involved private Double amount; //An uniqueness string number private String uniquness;
    //TODO - add bank’s signature 
    //TODO - add identity strings which contain the identity of the customer

--- src/Ecash.java
+++ src/Ecash.java
@@ -1,4 +1,12 @@
 
 public class Ecash {
+	
+	//The amount of the transaction involved
+	private Double amount;
+	//An uniqueness string number
+	private String uniquness;
+	
+	//TODO - add bank’s signature 
+	//TODO - add identity strings which contain the identity of the customer
 
 }
