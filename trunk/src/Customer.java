1cf4d3f81e863179b0c3d71942ec0aa1ed19871b
marbaugh@gmail.com <marbaugh@gmail.com@d4aabf46-09eb-ed3a-424b-fb80b3e2f78e>
2011-11-22 16:18:41 +0000

    Added an amount and submit button for the Customer Class in a GridLayout.

--- trunk/src/Customer.java
+++ trunk/src/Customer.java
@@ -1,10 +1,14 @@
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
 public class Customer extends JPanel implements ActionListener{
@@ -27,11 +31,22 @@ public class Customer extends JPanel implements ActionListener{
 	public void createAndShowGUI() {
 		// Create and set up the window
 		JFrame frame = new JFrame("Customer");
+		frame.setLayout(new GridLayout(2,1));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		frame.setMinimumSize(new Dimension(250, frame.getPreferredSize().height));
 		frame.setLocation((((int) Toolkit.getDefaultToolkit().getScreenSize()
 				.getWidth() - frame.getSize().width) / 2), 200);
+		
+	    JTextField amount = new JTextField("Amount:", 8);
+	    amount.setHorizontalAlignment(JTextField.CENTER);
+	    frame.add(amount);
+	    
+		JButton submit = new JButton("Submit");
+		submit.addActionListener(this);
+		submit.setActionCommand("submit");
+		submit.setToolTipText("Submit your purchase");
+		frame.add(submit);
 		
 		// Display the window
 		frame.pack();
