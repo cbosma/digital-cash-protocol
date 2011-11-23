b65b12c0d899d9c3c75796d841766461589470a3
jonathanpbuck@gmail.com <jonathanpbuck@gmail.com@d4aabf46-09eb-ed3a-424b-fb80b3e2f78e>
2011-11-23 00:40:42 +0000

    created a layout, added some text

--- trunk/src/Bank.java
+++ trunk/src/Bank.java
@@ -1,10 +1,13 @@
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
 
 public class Bank extends JPanel implements ActionListener{
@@ -28,10 +31,21 @@ public class Bank extends JPanel implements ActionListener{
 		// Create and set up the window
 		JFrame frame = new JFrame("Bank");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		frame.setMinimumSize(new Dimension(250, frame.getPreferredSize().height));
 		frame.setLocation((((int) Toolkit.getDefaultToolkit().getScreenSize()
 				.getWidth() - frame.getSize().width) / 2), 200);
+
+		Container contentPane = frame.getContentPane();
+		SpringLayout layout = new SpringLayout();
+		contentPane.setLayout(layout);
+		
+		JLabel title = new JLabel("The Bank");
+		contentPane.add(title);
+		layout.putConstraint(SpringLayout.NORTH, title, 5, SpringLayout.NORTH, contentPane);
+		layout.putConstraint(SpringLayout.WEST, title, 5, SpringLayout.WEST, contentPane);
+		layout.putConstraint(SpringLayout.EAST, contentPane, 5, SpringLayout.EAST, title);
+		layout.putConstraint(SpringLayout.SOUTH, contentPane, 5, SpringLayout.SOUTH, title);
 		
 		// Display the window
 		frame.pack();
