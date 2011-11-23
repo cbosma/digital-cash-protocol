804e0f962caf228814ae837abb42deb1372b8d58
jonathanpbuck@gmail.com <jonathanpbuck@gmail.com@d4aabf46-09eb-ed3a-424b-fb80b3e2f78e>
2011-11-23 01:27:47 +0000

    Added requirements as documentation

--- trunk/src/Bank.java
+++ trunk/src/Bank.java
@@ -9,7 +9,18 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SpringLayout;
 
-
+/**
+ * ===== Requirements =====
+ * - random choice of 1 out of N money orders sent by the
+ *   customer to remain unopened
+ * - an algorithm that certifies that all the N-1 money orders
+ *   have been filled with valid information
+ * - a procedure to certify that the orders received from
+ *   merchants have not been used previously and storage of the 
+ *   uniqueness string and identity strings of the orders in a
+ *   database file
+ * - Appropriate measures against reuse of the ecash
+ */
 public class Bank extends JPanel implements ActionListener{
 
 	/**
--- trunk/src/Ecash.java
+++ trunk/src/Ecash.java
@@ -1,12 +1,23 @@
-
+/**
+ * ===== Requirements =====
+ * The electronic cash (ecash) used during these transactions is a file
+ * which contains:
+ * - the amount of the transaction involved;
+ * - an uniqueness string number;
+ * - identity strings which contain the identity of the customer (this 
+ *   information remains secret unless the customer tries to use the ecash 
+ *   illicitly - more than once);
+ * - bank's signature (before the customer can use the ecash).
+ */
 public class Ecash {
-	
-	//The amount of the transaction involved
+
+	// The amount of the transaction involved
 	private Double amount;
-	//An uniqueness string number
-	private String uniquness;
-	
-	//TODO - add banks signature 
-	//TODO - add identity strings which contain the identity of the customer
+
+	// An uniqueness string number
+	private String uniqueness;
+
+	// TODO - add banks signature
+	// TODO - add identity strings which contain the identity of the customer
 
 }
