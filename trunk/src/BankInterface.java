cd078697f99a9cc06aef42061a8562c0c3d9f57f
jonathanpbuck@gmail.com <jonathanpbuck@gmail.com@d4aabf46-09eb-ed3a-424b-fb80b3e2f78e>
2011-12-08 23:43:17 +0000

    autoscroll bank status

--- trunk/src/BankInterface.java
+++ trunk/src/BankInterface.java
@@ -4,6 +4,8 @@ import java.awt.TextField;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.io.File;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
@@ -126,6 +128,11 @@ public class BankInterface extends JFrame implements ActionListener, WindowListe
 		scrollPane.setSize(500, 100);
 		status.setEditable(false);
 		scrollPane.setBorder(BorderFactory.createTitledBorder("Status"));
+		scrollPane.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener() {  
+			@Override
+			public void adjustmentValueChanged(AdjustmentEvent e) {  
+			e.getAdjustable().setValue(e.getAdjustable().getMaximum());  
+			}});  
 		contentPane.add(scrollPane);
 
 		// bind the top of the title to the top of the content pane
