000002b5f3a37aca1a722ea89d716675e267a148
marbaugh <marbaugh@d4aabf46-09eb-ed3a-424b-fb80b3e2f78e>
2011-12-08 21:42:01 +0000

    bank now opens socket for the merchant

--- src/BankServer.java
+++ src/BankServer.java
@@ -61,7 +61,7 @@ public class BankServer extends Thread{
 	 * Properties object that holds all account information
 	 */
 	static Properties accountProps = new Properties();
-	
+
 	/**
 	 * Randomly Generated Serial Version UID
 	 */
@@ -72,7 +72,7 @@ public class BankServer extends Thread{
 		readProperties();
 		setupSockets();
 	}
-	
+
 	/**
 	 * Open the properties file, generate a default if it doesn't exist.
 	 * The use of default settings is only for functionality proofing,
@@ -163,7 +163,7 @@ public class BankServer extends Thread{
 				if( compareMoneyOrders() == true ){
 					// Bank deducts the amount from their account
 					Double currBalance = Double.valueOf(accountProps.getProperty("customerBalance"));
-					
+
 					// Check to see the the Customer has enough money to complete the withdraw
 					if ( currBalance - (moneyOrderArrayFromCustomer[moneyOrderArrayFromCustomer.length-1].getAmount()) >= 0 ){
 						accountProps.setProperty("customerBalance", String.valueOf((currBalance - (moneyOrderArrayFromCustomer[moneyOrderArrayFromCustomer.length-1].getAmount()))));
@@ -182,148 +182,196 @@ public class BankServer extends Thread{
 							out.flush();
 							System.out.println("Sent Signed Money Order back to Customer");
 							BankInterface.status.append("\nSent Signed Money Order back to Customer");
-							
+
 							in.close();
 							out.close();
 							connection.close();
 							providerSocket.close();
+
+							//1. creating a server socket for merchant
+							providerSocket = new ServerSocket(2006, 10);
+							//2. Wait for connection
+							System.out.println("Waiting for connection from Merchant...");
+							BankInterface.status.append("\nWaiting for connection from Merchant...");
+							connection = providerSocket.accept();
+							System.out.println("Connection received from Customer at " + connection.getInetAddress().getHostName());
+							//3. Get Input Stream
+							in = new ObjectInputStream(connection.getInputStream());
+							//4. The two parts communicate via the input and output streams
+							signedObject = (SignedObject) in.readObject();
+							PublicKey publicKey;
+							try {
+								publicKey = (PublicKey) readFromFile("publicKey.dat");
+								Signature sig = Signature.getInstance("DSA");
+								if (signedObject.verify(publicKey, sig)){
+									System.out.println("Bank has checked, and the Banks Signature is good");
+									MerchantInterface.status.append("Bank has checked, and the Banks Signature is good");
+								}
+
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+
+							}
+							else{
+								System.out.println("Bank could not sign the Money Order");
+								BankInterface.status.append("\nBank could not sign the Money Order");							
+							}
 						}
 						else{
-							System.out.println("Bank could not sign the Money Order");
-							BankInterface.status.append("\nBank could not sign the Money Order");							
+							System.out.println("Customer does not have sufficient funds");
+							BankInterface.status.append("\nCustomer does not have sufficient funds");							
 						}
 					}
-					else{
-						System.out.println("Customer does not have sufficient funds");
-						BankInterface.status.append("\nCustomer does not have sufficient funds");							
-					}
+				}
+				catch(ClassNotFoundException classnot){
+					System.err.println("Data received in unknown format");
 				}
 			}
-			catch(ClassNotFoundException classnot){
-				System.err.println("Data received in unknown format");
+
+			catch(IOException ioException){
+				System.out.println("Error With Socket Connection");
+				ioException.printStackTrace();
 			}
 		}
 
-		catch(IOException ioException){
-			System.out.println("Error With Socket Connection");
-			ioException.printStackTrace();
-		}
-	}
-
-	public static Boolean signMoneyOrder(Ecash ecashToSign){
-		// The Bank is satisfied that Customer did not make any attempts to cheat, so sign the one remaining money order 
-		try {
-
-			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA");
-			keyGen.initialize(1024);
-			KeyPair keypair = keyGen.genKeyPair();
-			PrivateKey privateKey = keypair.getPrivate();
-			PublicKey publicKey = keypair.getPublic();
+		public static Boolean signMoneyOrder(Ecash ecashToSign){
+			// The Bank is satisfied that Customer did not make any attempts to cheat, so sign the one remaining money order 
 			try {
-				writeToFile("publicKey.dat", publicKey);
-			} catch (Exception e) {
+
+				KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA");
+				keyGen.initialize(1024);
+				KeyPair keypair = keyGen.genKeyPair();
+				PrivateKey privateKey = keypair.getPrivate();
+				PublicKey publicKey = keypair.getPublic();
+				try {
+					writeToFile("publicKey.dat", publicKey);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+
+				Signature sig = Signature.getInstance(privateKey.getAlgorithm());
+				signedObject = new SignedObject(ecashToSign, privateKey, sig);
+				sig = Signature.getInstance(publicKey.getAlgorithm());
+				if ( signedObject.verify(publicKey, sig) ){
+					return true;
+				}
+				else{
+					return false;
+				}
+			} 
+
+			catch (NoSuchAlgorithmException e) {
+				System.err.println("Error Signing Ecash Object");
+				e.printStackTrace();
+			} catch (InvalidKeyException e) {
+				System.err.println("Error Signing Ecash Object");
+				e.printStackTrace();
+			} catch (SignatureException e) {
+				System.err.println("Error Signing Ecash Object");
+				e.printStackTrace();
+			} catch (IOException e) {
+				System.err.println("Error Signing Ecash Object");
 				e.printStackTrace();
 			}
-
-
-			Signature sig = Signature.getInstance(privateKey.getAlgorithm());
-			signedObject = new SignedObject(ecashToSign, privateKey, sig);
-			sig = Signature.getInstance(publicKey.getAlgorithm());
-			if ( signedObject.verify(publicKey, sig) ){
-				return true;
-			}
-			else{
-				return false;
-			}
-		} 
-
-		catch (NoSuchAlgorithmException e) {
-			System.err.println("Error Signing Ecash Object");
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			System.err.println("Error Signing Ecash Object");
-			e.printStackTrace();
-		} catch (SignatureException e) {
-			System.err.println("Error Signing Ecash Object");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.err.println("Error Signing Ecash Object");
-			e.printStackTrace();
+			return false;
 		}
-		return false;
-	}
 
-	public static Boolean compareMoneyOrders(){
-		// The bank checks the amount of n-1 money orders
-		// Open n-1 money orders and see that they all have the same amount
-		for ( int i = 0; i < moneyOrderArrayFromCustomer.length -2; i++ ){
+		public static Boolean compareMoneyOrders(){
+			// The bank checks the amount of n-1 money orders
+			// Open n-1 money orders and see that they all have the same amount
+			for ( int i = 0; i < moneyOrderArrayFromCustomer.length -2; i++ ){
 
-			// Save the first amount and uniqueness string to compare to the others
-			if(i == 0){
-				if ( moneyOrderArrayFromCustomer[i].getAmount() != null ){
-					moneyOrderAmount= moneyOrderArrayFromCustomer[i].getAmount();					
-				}
-				if( moneyOrderArrayFromCustomer[i].getUniqueness() != null ){
-					uniqueness[i] = moneyOrderArrayFromCustomer[i].getUniqueness();
-				}
-			}
-			// Compare all other amounts and uniqueness strings  to the first
-			else{
-				// If there is a mismatch then set the matching boolean to false
-				if( moneyOrderArrayFromCustomer[i].getAmount() != moneyOrderAmount){
-					System.err.println("The amounts do not match for " + moneyOrderArrayFromCustomer[i].getAmount() + " and " + moneyOrderAmount);
-					BankInterface.status.append("\nThe amounts do not match for " + moneyOrderArrayFromCustomer[i].getAmount() + " and " + moneyOrderAmount);
-					matchingAmounts = false;
-				}
-				// If any two uniqueness string match set the matching unuqieness to true
-				if ( uniqueness != null){
-					tmpUniqueness = false;
-					for ( int j = 0; j < uniqueness.length; j++ ){
-						if ( uniqueness[j] != null){
-							if ( uniqueness[j].compareTo(moneyOrderArrayFromCustomer[i].getUniqueness()) == 0 ){
-								matchingUniqueness = true;
-								tmpUniqueness = true;
-								System.err.println("The two Uniqueness Strings " + uniqueness[j].toString() + " are the same, you are cheating!");
-								BankInterface.status.append("\nThe two Uniqueness Strings " + uniqueness[j].toString() + " are the same, you are cheating!");
-							}	
-						}
+				// Save the first amount and uniqueness string to compare to the others
+				if(i == 0){
+					if ( moneyOrderArrayFromCustomer[i].getAmount() != null ){
+						moneyOrderAmount= moneyOrderArrayFromCustomer[i].getAmount();					
 					}
-					if ( tmpUniqueness == false){
+					if( moneyOrderArrayFromCustomer[i].getUniqueness() != null ){
 						uniqueness[i] = moneyOrderArrayFromCustomer[i].getUniqueness();
 					}
 				}
+				// Compare all other amounts and uniqueness strings  to the first
+				else{
+					// If there is a mismatch then set the matching boolean to false
+					if( moneyOrderArrayFromCustomer[i].getAmount() != moneyOrderAmount){
+						System.err.println("The amounts do not match for " + moneyOrderArrayFromCustomer[i].getAmount() + " and " + moneyOrderAmount);
+						BankInterface.status.append("\nThe amounts do not match for " + moneyOrderArrayFromCustomer[i].getAmount() + " and " + moneyOrderAmount);
+						matchingAmounts = false;
+					}
+					// If any two uniqueness string match set the matching unuqieness to true
+					if ( uniqueness != null){
+						tmpUniqueness = false;
+						for ( int j = 0; j < uniqueness.length; j++ ){
+							if ( uniqueness[j] != null){
+								if ( uniqueness[j].compareTo(moneyOrderArrayFromCustomer[i].getUniqueness()) == 0 ){
+									matchingUniqueness = true;
+									tmpUniqueness = true;
+									System.err.println("The two Uniqueness Strings " + uniqueness[j].toString() + " are the same, you are cheating!");
+									BankInterface.status.append("\nThe two Uniqueness Strings " + uniqueness[j].toString() + " are the same, you are cheating!");
+								}	
+							}
+						}
+						if ( tmpUniqueness == false){
+							uniqueness[i] = moneyOrderArrayFromCustomer[i].getUniqueness();
+						}
+					}
+				}
+			}
+			if ( matchingAmounts == true && matchingUniqueness == false ){
+				System.out.println("All amounts matched and All Uniqueness String are different!");
+				BankInterface.status.append("\nAll amounts matched and All Uniqueness String are different!");
+				return true;
+			}
+			else{
+				System.err.println("The amounts did not match or Two Uniqueness String are the same, you are cheating!");
+				BankInterface.status.append("\nThe amounts did not match or Two Uniqueness String are the same, you are cheating!");
+				return false;
+			}	
+		}
+
+		private static void writeToFile(String filename, Object object) throws Exception {
+			FileOutputStream fos = null;
+			ObjectOutputStream oos = null;
+
+			try {
+				fos = new FileOutputStream(new File(filename));
+				oos = new ObjectOutputStream(fos);
+				oos.writeObject(object);
+				oos.flush();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if (oos != null) {
+					oos.close();
+				}
+				if (fos != null) {
+					fos.close();
+				}
 			}
 		}
-		if ( matchingAmounts == true && matchingUniqueness == false ){
-			System.out.println("All amounts matched and All Uniqueness String are different!");
-			BankInterface.status.append("\nAll amounts matched and All Uniqueness String are different!");
-			return true;
+
+		private static Object readFromFile(String filename) throws Exception {
+			FileInputStream fis = null;
+			ObjectInputStream ois = null;
+			Object object = null;
+
+			try {
+				fis = new FileInputStream(new File(filename));
+				ois = new ObjectInputStream(fis);
+				object = ois.readObject();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if (ois != null) {
+					ois.close();
+				}
+				if (fis != null) {
+					fis.close();
+				}
+			}
+			return object;
 		}
-		else{
-			System.err.println("The amounts did not match or Two Uniqueness String are the same, you are cheating!");
-			BankInterface.status.append("\nThe amounts did not match or Two Uniqueness String are the same, you are cheating!");
-			return false;
-		}	
+
 	}
-
-    private static void writeToFile(String filename, Object object) throws Exception {
-        FileOutputStream fos = null;
-        ObjectOutputStream oos = null;
-
-        try {
-            fos = new FileOutputStream(new File(filename));
-            oos = new ObjectOutputStream(fos);
-            oos.writeObject(object);
-            oos.flush();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (oos != null) {
-                oos.close();
-            }
-            if (fos != null) {
-                fos.close();
-            }
-        }
-    }
-}
